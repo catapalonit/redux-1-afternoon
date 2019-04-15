c4428339d92791147b0e6d0fcf30b9191b437d55
Bryan Hawkins <bhawkins3729@gmail.com>
2019-04-15 15:52:27 -0500

    second commit

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { ADD_INSTRUCTION } from "./../../store"
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState()
     this.state = {
-      instructions: [],
+      instructions: reduxState.instructions,
       input: ""
     };
   }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      const reduxState = store.getState()
+      this.setState({
+        instructions: reduxState.instructions
+      })
+    })
+  }
+
+
   handleChange(val) {
     this.setState({
       input: val
     });
   }
   addInstruction() {
-    // Send data to Redux state
+    store.dispatch({
+      type: ADD_INSTRUCTION,
+      payload: this.state.input
+    })
     this.setState({
       input: ""
     });
--- src/store.js
+++ src/store.js
@@ -1,1 +1,39 @@
 
+import { createStore } from "redux"
+
+const initialState = {
+    name: "",
+    category: "",
+    author_first: "",
+    author_last: "",
+    ingredients: [],
+    instructions: []
+}
+
+export const UPDATE_NAME = "UPDATE_NAME"
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY"
+export const UPDATE_AUTHOR_FIRST = "UPDATE_AUTHOR_FIRST"
+export const UPDATE_AUTHOR_LAST = "UPDATE_AUTHOR_LAST"
+export const ADD_INGREDIENT = "ADD_INGREDIENTS"
+export const ADD_INSTRUCTION = "ADD_INSTRUCTION"
+
+
+function reducer(state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case UPDATE_NAME: return { ...state, name: payload }
+        case UPDATE_CATEGORY: return { ...state, category: payload }
+        case UPDATE_AUTHOR_FIRST: return { ...state, category: payload }
+        case UPDATE_AUTHOR_LAST: return { ...state, category: payload }
+        case ADD_INGREDIENT:
+            const newIngredients = [...state.ingredients, payload]
+            return { ...state, ingredients: newIngredients }
+        case ADD_INSTRUCTION:
+            const newInstruction = [...state.ingredients, payload]
+            return { ...state, instructions: newInstructions }
+
+        default: return state;
+    }
+}
+
+export default createStore(reducer)
